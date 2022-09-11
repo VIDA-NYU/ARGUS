d8b57458ce5b570fae40a9a26e5278836baa0988
Bea Steers <bea.steers@gmail.com>
2022-09-11 17:26:06 -0400

    add websocket url to config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
-const API_URL = 'https://api.ptg.poly.edu';
-const RECORDINGS_STATIC_PATH = '/recordings/static/';
-const TEST_USER = 'test';
-const TEST_PASS = 'test';
+export const API_URL = 'https://api.ptg.poly.edu';
+export const RECORDINGS_STATIC_PATH = '/recordings/static/';
+export const TEST_USER = 'test';
+export const TEST_PASS = 'test';
+export const WS_API_URL = API_URL.replace('https://', 'wss://');
 
-export {API_URL, RECORDINGS_STATIC_PATH, TEST_USER, TEST_PASS};
+// export {API_URL, RECORDINGS_STATIC_PATH, TEST_USER, TEST_PASS};
