efe95063c3b567fd52816ad98db6bc23e6044937
Erin McGowan <erinmcgowan477@gmail.com>
2023-03-20 16:35:45 -0400

    fix map mistake

--- frontend/src/components/PointCloudViewer/model/renderables/imu/imu-vectors.ts
+++ frontend/src/components/PointCloudViewer/model/renderables/imu/imu-vectors.ts
@@ -1,4 +1,4 @@
-import {rpy21, rpy12, get_sec_from_start_arr, get_rpy_arr, get_all_rms, get_rotated_imu_vectors} from "../utils";
+import {rpy21, rpy12, get_sec_from_start_arr, get_rpy_arr, get_all_rms, get_rotated_imu_vectors} from "../utils.ts";
 
 let gaze_data = sceneData.pointCloudData["gaze"]
 let imu_accel_arr = sceneData.IMUAccelData
@@ -6,12 +6,20 @@ let imu_gyro_arr = sceneData.IMUGyroData
 
 
 //Sample accel/gyro arrs to be the same length as eye gaze (one accel/gyro value for each timestep)
-let imu_accel_arr_sampled = imu_accel_arr.map((obj, index) => (imu_accel_arr[index * Math.floor(imu_accel_arr.length/gaze_data.length)]));
+let imu_accel_arr_sampled = []
+for (let i = 0; i < gaze_data.length; i++){
+    imu_accel_arr_sampled.push(imu_accel_arr[i * Math.floor(imu_accel_arr.length/gaze_data.length)])
+}
 
-let imu_gyro_arr_sampled = imu_gyro_arr.map((obj, index) => (imu_gyro_arr[index * Math.floor(imu_gyro_arr.length/gaze_data.length)]));
-
+let imu_gyro_arr_sampled = []
+for (let i = 0; i < gaze_data.length; i++){
+  imu_gyro_arr_sampled.push(imu_gyro_arr[i * Math.floor(imu_gyro_arr.length/gaze_data.length)])
+}
 //Match accel values with head position (eye gaze origin)
-let imu_vector_arr = gaze_data.map((obj, index) => ([gaze_data[index]['GazeOrigin']['x'], gaze_data[index]['GazeOrigin']['y'], gaze_data[index]['GazeOrigin']['z'], gaze_data[index]['GazeOrigin']['x'] + imu_accel_arr_sampled[index][0], gaze_data[index]['GazeOrigin']['y'] + imu_accel_arr_sampled[index][1], gaze_data[index]['GazeOrigin']['z'] + imu_accel_arr_sampled[index][2]]));
+let imu_vector_arr = []
+for (let i = 0; i < gaze_data.length; i++){
+    imu_vector_arr.push([gaze_data[i]['GazeOrigin']['x'], gaze_data[i]['GazeOrigin']['y'], gaze_data[i]['GazeOrigin']['z'], gaze_data[i]['GazeOrigin']['x'] + imu_accel_arr_sampled[i][0], gaze_data[i]['GazeOrigin']['y'] + imu_accel_arr_sampled[i][1], gaze_data[i]['GazeOrigin']['z'] + imu_accel_arr_sampled[i][2]])
+}
 
 //get seconds from start and roll pitch yaw arrays
 let sfs_arr = get_sec_from_start_arr(gaze_data)
